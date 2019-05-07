85f8d5f2a85d4ea5de57b273b93dc53387067547
katjadulmaine <katja.dulmaine@gmail.com>
2019-05-07 10:16:28 -0400

    stash

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser')
 var app = express();
-var apiR = require("./app/routing/apiRoutes.js");
-var htmlR = require("./app/routing/htmlRoutes.js");
 var friends = require("./app/data/friends");
 
-// load routes into app by passing app into route functions
-// htmlR(app);
-// apiR(app);
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(__dirname + "/app/public"));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/app/public/index.html"));
 });
-
-
-
-
-  
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/app/public/survey.html"));
     
