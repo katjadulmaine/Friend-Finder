de8b0826aac7eac72df44fd521d3fd8bf9383fbe
Katja Dulmaine <KatjaDulmaine@Mamas-iMac-2.local>
2019-05-30 00:43:57 -0400

    modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -22,23 +22,8 @@ module.exports = function (app) {
                 var friendsScore = res.json(data);
             }
         })
-        app.get("/api/input", function (err, data) {
-            // var newFriend =  data.input {
-            // "name": $("#name").val().trim(),
-            // "photo": $("#photo").val().trim(),
-            // "scores": [
-            //       parseInt($( "#q0" ).val()),
-            //       parseInt($( "#q1" ).val()),
-            //       parseInt($( "#q2" ).val()),
-            //       parseInt($( "#q3" ).val()),
-            //       parseInt($( "#q4" ).val()),
-            //       parseInt($( "#q5" ).val()),
-            //       parseInt($( "#q6" ).val()),
-            //       parseInt($( "#q7" ).val()),
-            //       parseInt($( "#q8" ).val()),
-            //       parseInt($( "#q9" ).val()),
-            //       parseInt($( "#q10" ).val())
-            //   ]}; console.log(newFriend);
+        var newFriendScore = req.body.scores
+     
         });
         var space = 0;
         var spaceArr = [];
