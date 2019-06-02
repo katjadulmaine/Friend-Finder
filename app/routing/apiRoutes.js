f26f90d4eb053ceca20049a052d2bf2c4c7f3294
katjadulmaine <katja.dulmaine@gmail.com>
2019-06-02 16:49:56 -0400

    fixed score

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,40 +10,39 @@ module.exports = function (app) {
         res.json(friends);
         console.log(req.body);
     })
-
+    
 
     app.post("/api/friends", function (req, res) {
-        fs.readFile('app/data/friends.js', function (err, data) {
-            if (err) {
-                return console.log(err);
-            } else {
-                var friendsScore = res.json(data);
-            }
-        })
-        var addFriend = req.body.scores
-     
-        });
-        var space = 0;
-        var spaceArr = [];
-        scoreNew = addFriend.scores.val
+
+        var addFriend = req.body
+        console.log(addFriend)
+        var spaceBetween = 0;
+        var bestScore = 100
+        var scoreNew = addFriend.scores
+        var nBestFriend
+        console.log("scorenew",scoreNew)
         for (var i = 0; i < friends.length; i++) {
 
             for (var n = 0; n < scoreNew.length; n++) {
-                space += (Math.abs(parseInt(friends[i].scores[n]) - parseInt(scoreNew[n])));
-
+                spaceBetween += (Math.abs(parseInt(friends[i].scores[n]) - parseInt(scoreNew[n])));
             }
-            spaceArr.push(space);
-        }
-        for (var s = 0; s < spaceArr.length; s++) {
-            if (spaceArr[s] <= scoreArr[best]) {
-                best = s;
+            if (spaceBetween < bestScore){
+ 
+                bestScore = spaceBetween
+                nBestFriend= i
             }
+           spaceBetween=0
         }
-        var bestFriend = friends[best]
-        res.json(bestFriend);
-
-    });
+        // for (var s = 0; s < spaceArr.length; s++) {
+        //     if (spaceArr[s] <= scoreName[best]) {
+        //         best = s;
+        //     }
+        // }
+        friends.push(addFriend)
+        res.json({name:friends[nBestFriend].name,photo:friends[nBestFriend].photo})
+    })
 };
+
   //display best match
 //   app.get("/app/friends/:match", function(req, res){
 //     var match = req.params.match;
